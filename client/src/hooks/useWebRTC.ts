c9154a52bae3378715c516a05e67a88025a3b1f6
Mohamedzahangir <mohamedzahangiralimolla@gmail.com>
2026-08-16 00:49:44 +0530

    Add TURN servers to fix strict NAT blocking

--- client/src/hooks/useWebRTC.ts
+++ client/src/hooks/useWebRTC.ts
@@ -51,7 +51,22 @@ export function useWebRTC({ ws, sessionId, participantId, cameraOn, micOn }: Web
       const newPc = new RTCPeerConnection({
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
-          { urls: 'stun:global.stun.twilio.com:3478' }
+          { urls: 'stun:global.stun.twilio.com:3478' },
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
         ]
       });
       
